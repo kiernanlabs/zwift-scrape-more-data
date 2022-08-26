28c70c5c8e1d52bbf72fb753bfb61be18894b9c4
Joe Kiernan <joe@kiernanlabs.com>
2022-08-26 19:28:53 -0400

    cleaned up selenium deprecation

--- zwift_events copy.py
+++ zwift_events copy.py
@@ -10,12 +10,9 @@ from argparse import ArgumentParser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
-from webdriver_manager.firefox import GeckoDriverManager
-
 from credentials import *
 import time
 import zwift_scrape
@@ -26,7 +23,16 @@ def getEventURLs(urlpage, headless=False):
     if headless:
         opts.headless = True
     scraped_data = {} 
-    with webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=opts
+    platform = sys.platform
+    driverPath = "./drivers/geckodriver-{}".format(platform)
+    if platform == "win32":
+        driverPath += ".exe"
+    elif platform in ["linux", "darwin"]:
+        chmod(driverPath, S_IXUSR | S_IWUSR | S_IRUSR)
+    with webdriver.Firefox(
+        executable_path=driverPath,
+        service_log_path="./drivers/logs/geckodriver-{}_log.log".format(platform),
+        options=opts,
     ) as driver:
         driver.implicitly_wait(10)
         print("Scraping data from: {}.".format(urlpage))
--- zwift_scrape.py
+++ zwift_scrape.py
@@ -10,9 +10,11 @@ from argparse import ArgumentParser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
+from webdriver_manager.firefox import GeckoDriverManager
 from credentials import *
 
 
@@ -21,16 +23,8 @@ def scrape(urlpage, headless=False):
     if headless:
         opts.headless = True
     scraped_data = {} 
-    platform = sys.platform
-    driverPath = "./drivers/geckodriver-{}".format(platform)
-    if platform == "win32":
-        driverPath += ".exe"
-    elif platform in ["linux", "darwin"]:
-        chmod(driverPath, S_IXUSR | S_IWUSR | S_IRUSR)
-    with webdriver.Firefox(
-        executable_path=driverPath,
-        service_log_path="./drivers/logs/geckodriver-{}_log.log".format(platform),
-        options=opts,
+    
+    with webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=opts
     ) as driver:
         driver.implicitly_wait(10)
         for n, url in enumerate(urlpage):
